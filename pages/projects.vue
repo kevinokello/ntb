c438cd04f569ee610ce92ad0f01ffdaef7a505ce
kevinokello <okellokevin77@gmail.com>
2022-10-22 10:34:16 +0300

    projects

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
     <div class="pt-6 pb-8 space-y-2 md:space-y-5">
-      <h1
-        class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
-      >
-        Projects
-      </h1>
       <p class="text-lg leading-7 text-gray-500 dark:text-gray-400">
         Some of the projects I've build or been a part of.
       </p>
@@ -21,9 +16,6 @@
               :description="item.description"
               :href="item.href"
               :github="item.github"
-              :tech1="item.tech1"
-              :tech2="item.tech2"
-              :tech3="item.tech3"
             ></project-card>
           </div>
         </div>
